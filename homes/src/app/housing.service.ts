079dcbf2d0e2f9324c896a008914ce56f1dff860
Joshua Lanctot <joshua.lanctot10@gmail.com>
2023-06-22 15:56:31 -0500

    homes tutorial done

--- homes/src/app/housing.service.ts
+++ homes/src/app/housing.service.ts
@@ -1,20 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HousingLocation } from './housinglocation';
-import { housingLocations } from './housingLocations';
+// import { housingLocations } from './housingLocations';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HousingService {
-  housingLocationList = housingLocations;
+  // housingLocationList = housingLocations;
+  url = 'http://localhost:3000/locations'
 
   constructor() { }
 
-  getAllHousingLocations(): HousingLocation[] {
-    return this.housingLocationList;
+  async getAllHousingLocations(): Promise<HousingLocation[]> {
+    const data = await fetch(this.url);
+    return await data.json() ?? [];
   }
 
-  getHousingLocationById(id: number): HousingLocation | undefined {
-    return this.housingLocationList.find(housingLocation => housingLocation.id === id);
+  async getHousingLocationById(id: number): Promise<HousingLocation | undefined> {
+    const data = await fetch(`${this.url}/${id}`);
+    return await data.json() ?? {};
+  }
+
+  submitApplication(firstName: string, lastName: string, email: string){
+    console.log(`Homes application received: ${firstName} ${lastName} ${email}`)
   }
 }
